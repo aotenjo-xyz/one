a8b9c1100b833c296b802b50890516e387779e43
Yuichiro Aoki <45054071+yuichiroaoki@users.noreply.github.com>
2025-07-18 16:09:02 +0900

    add MT6701 example

---
+++ examples/MT6701/README.md
@@ -0,0 +1,6 @@
+# Magnetic Encoder MT6701 Example
+Aotenjo One board uses MT6701 magnetic encoder to measure the motor position and velocity. This example demonstrates how to use the MT6701 encoder with the Aotenjo One board.
+
+MT6701 is a 14-bit magnetic encoder with SPI interface. It provides high precision and fast response time, making it suitable for gimbal motor applications.
+
+[MT6701 Datasheet](https://uploadcdn.oneyac.com/attachments/files/brand_pdf/magntek/F3/CA/MT6701QT-STD.pdf) 
---
+++ examples/MT6701/main.cpp
@@ -0,0 +1,73 @@
+#include "SimpleFOC.h"
+#include "SimpleFOCDrivers.h"
+#include "SPI.h"
+#include "encoders/mt6701/MagneticSensorMT6701SSI.h"
+#include "Arduino.h"
+
+#define SENSOR1_CS PA4
+MagneticSensorMT6701SSI sensor1(SENSOR1_CS);
+
+HardwareSerial Serial1(PA3, PA2);
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+   */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+   * in the RCC_OscInitTypeDef structure.
+   */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;
+  RCC_OscInitStruct.PLL.PLLN = 85;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
+    Error_Handler();
+  }
+}
+
+void setup() {
+  Serial1.begin(115200);
+
+  sensor1.init();
+  Serial1.println("Sensor ready");
+  _delay(1000);
+}
+
+void loop() {
+  // iterative function updating the sensor internal variables
+  // it is usually called in motor.loopFOC()
+  // this function reads the sensor hardware and 
+  // has to be called before getAngle nad getVelocity
+  sensor1.update();
+  
+  // display the angle and the angular velocity to the terminal
+  Serial1.print(sensor1.getAngle());
+  Serial1.print("\t");
+  Serial1.println(sensor1.getVelocity());
+}

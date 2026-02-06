41dbedb671152eaaa376fcee2f43849c64fbd24f
Yuichiro Aoki <yuichiroaoki27@gmail.com>
2026-02-06 23:56:22 +0900

    Remove unused PARSE_RESULT enum from STM32CAN.h

--- lib/STM32CAN/STM32CAN.h
+++ lib/STM32CAN/STM32CAN.h
@@ -1,13 +1,6 @@
 #include <Arduino.h>
 #include <assert.h>
 
-typedef enum {
-  ANG_CONTROL = 0,
-  REQ_DATA,
-  EMERGENCY_STOP,
-  INVALID_COMMAND
-} PARSE_RESULT;
-
 void packAngleIntoCanMessage(uint8_t *message, float angle);
 float unpackAngleFromCanMessage(const uint8_t *data);
 uint32_t GetFDCANDataLengthCode(uint8_t bytes);
